d0da10e285d3d622f2dcc872de0be885f59915ea
192425183simats-spec <192425183.simats@saveetha.com>
2026-07-14 09:43:51 +0530

    Add files via upload

---
+++ cv exp1.py
@@ -0,0 +1,19 @@
+import cv2
+img = cv2.imread(r"C:\Users\Harshitha\Downloads\HappyFish.jpg")
+if img is None:
+    print("Error: Image not found!")
+else:
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    
+    cv2.imshow("Original Image", img)
+
+    
+    cv2.imshow("Grayscale Image", gray)
+
+    
+    cv2.waitKey(0)
+
+   
+    cv2.destroyAllWindows()
